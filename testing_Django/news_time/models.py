58ddfe400e7a551bbb2a21d4efacc0aa1bf60444
IgorL0SEV <igor.v.losev@gmail.com>
2024-11-08 23:58:13 +0300

    final code

--- testing_Django/news_time/models.py
+++ testing_Django/news_time/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class News_post(models.Model):
+    title = models.CharField('Название новости', max_length=50)
+    author = models.CharField('Автор новости', max_length=50)
+    pub_date = models.DateTimeField('Дата публикации')
+    short_description = models.CharField('Краткое описание новости', max_length=200)
+    text = models.TextField('Новость')
+
+
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
--- testing_Django/news_time/views.py
+++ testing_Django/news_time/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from .models import News_post
 
 # Create your views here.
 
 def home(request):
-    return render (request, 'main/news.html')
+    news = News_post.objects.all()
+    return render(request, 'news_time/news_time.html', {'news': news})
+
+
 
