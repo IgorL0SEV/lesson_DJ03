750897641c8300a7c20ea63762bae5c57c4df7b8
IgorL0SEV <igor.v.losev@gmail.com>
2024-11-09 13:42:55 +0300

    add cod for new form

--- testing_Django/news_time/urls.py
+++ testing_Django/news_time/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home_news_time')
+    path('', views.home, name='home_news_time'),
+    path('create_news/', views.create_news, name='add_news')
 ]
--- testing_Django/news_time/views.py
+++ testing_Django/news_time/views.py
@@ -7,5 +7,6 @@ def home(request):
     news = News_post.objects.all()
     return render(request, 'news_time/news_time.html', {'news': news})
 
-
+def create_news(request):
+    return render(request, 'news_time/add_new_post.html')
 
