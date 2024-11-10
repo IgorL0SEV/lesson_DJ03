57953ce41edce7fefbfc64722eb0517848ed07ed
IgorL0SEV <igor.v.losev@gmail.com>
2024-11-10 20:47:50 +0300

    final lesson code

--- testing_Django/news_time/urls.py
+++ testing_Django/news_time/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home_news_time'),
-    path('create_news', views.create_news, name='add_news')
+    path('create_news/', views.create_news, name='add_news')
 ]
--- testing_Django/news_time/views.py
+++ testing_Django/news_time/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import News_post
 from .forms import News_postForm
 
@@ -9,7 +9,16 @@ def home(request):
     return render(request, 'news_time/news_time.html', {'news': news})
 
 def create_news(request):
+    error = ""
+    if request.method == 'POST':
+        form = News_postForm(request.POST)  # Сюда сохранится информация от пользователя.
+        if form.is_valid():
+            form.save()
+            return redirect('home_news_time')
+        else:
+            error = "Данные были заполнены некорректно"
+
     form = News_postForm()
-    return render(request, 'news_time/add_new_post.html', {'form': form})
+    return render(request, 'news_time/add_new_post.html', {'form': form, 'error': error} )
 
 
